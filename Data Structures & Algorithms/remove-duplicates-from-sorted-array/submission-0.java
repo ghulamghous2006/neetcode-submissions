078def1ebe9e8b271b04740a5361b5862f9ac876
GHULAM GHOUS <147304795+ghulamghous2006@users.noreply.github.com>
2026-09-24 23:51:36 +0530

    Bulk sync: 27 submissions

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = 1;
+
+        while(right < nums.length){
+            if(nums[left] != nums[right]){
+                left++;
+                nums[left] = nums[right];
+            }
+            right++;
+        }
+        return left + 1;
+      
+        
+    }
+}
